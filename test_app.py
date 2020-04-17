ee01b19d25db06772f0447d4892b88665c9f3910
fabio.rodrigues <sorcerer86pt@gmail.com>
2020-04-17 16:58:43 +0100

    add new test and fix format

--- test_app.py
+++ test_app.py
@@ -1,14 +1,21 @@
 from challenge import getMultiplicationValues
 
+
 def test_sample_array():
-    numbers = [6,8,8,7,2,5]
+    numbers = [6, 8, 8, 7, 2, 5]
     mult_values = getMultiplicationValues(numbers)
     assert (int(mult_values[0])) == 6
     assert (int(mult_values[1])) == 8
-    
+
+
 def test_second_sample_array():
-    numbers = [1,9,2,4]
+    numbers = [1, 9, 2, 4]
     mult_values = getMultiplicationValues(numbers)
     assert (int(mult_values[0])) == 9
     assert (int(mult_values[1])) == 4
     
+def test_third_sample_array():
+    numbers = [1, 2, 3, 4, 5, 6]
+    mult_values = getMultiplicationValues(numbers)
+    assert (int(mult_values[0])) == 6
+    assert (int(mult_values[1])) == 5
